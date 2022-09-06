29c6ee38d5ac3a397697c5cba97441c6531639fc
harishaa827 <>
2022-09-07 00:52:02 +0530

    792022

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 
-const notes = [
+const d = new Date()
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -24,10 +25,72 @@ const notes = [
     }
 ]
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
+const info = `<p>Phonebook has info for ${persons.length} people</p>
+              ${d}`
+
+
+const generateID = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
   })
 
+app.get('/', (request, response)=> {
+  response.send("<h1>Hello World</h1>")
+})
+
+app.get("/info", (request, response)=> {
+  response.send(info)
+})
+
+app.get("/api/persons/:id", (request, response)=> {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if(person == null) {
+    response.status(400).json({
+      error:"Person Missing"
+    })
+  }
+  else {
+    response.json(person)
+  }
+})
+
+app.delete("/api/persons/:id", (request, response)=> {
+  const id = Number(request.params.id)
+  persons  = persons.filter(person => person.id!==id)
+  response.status(204).end()
+})
+
+app.post("api/persons", (request, response)=> {
+  
+  const body = request.body
+  console.log(body)
+  if(!body.name){
+    response.status(400).json({
+      error: "Name missing"
+    })
+  }
+  if(!body.number){
+    response.status(400).json({
+      error: "Number missing"
+    })
+  }
+
+  const person = {
+    id: generateID(1,1000),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
 
 
 const PORT = 3001
